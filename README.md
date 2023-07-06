e2da43a2060217f8a79c1ecf6bf91e4e66e6a0f7
Sergey.Ablaev <Sergey.Ablaev@x5.ru>
2023-07-06 23:52:09 +0300

    refactoring

---
+++ README.md
@@ -0,0 +1,5 @@
+# 
+Запуск проекта осуществляется в классе 'RestApiDemoApplication'
+(файл конфигурации для запуска сохранен в .run/RestApiDemoApplication.run.xml)
+
+Юнит тест находится в 'restrictionApiTest'
--- src/main/java/com/example/restapidemo/unit/rest/api/RestrictionApi.java
+++ src/main/java/com/example/restapidemo/unit/rest/api/RestrictionApi.java
@@ -6,8 +6,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 public interface RestrictionApi {
     /**
